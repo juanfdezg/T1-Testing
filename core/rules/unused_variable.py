24c36e483f638ebacf4ce4dcf988f8e0753a1b66
IgnacioMejias <you@example.com>
2023-09-04 17:47:27 -0300

    unused ready

--- core/rules/unused_variable.py
+++ core/rules/unused_variable.py
@@ -1,8 +1,9 @@
 from ..rule import *
 import ast
 
-class UnusedVariableVisitor(WarningNodeVisitor):
+class UnusedVariableVisitor(ast.NodeVisitor):
     def __init__(self):
+        super().__init__()
         self.assigned_variables = {}
         self.used_variables = set()
 
@@ -17,33 +18,31 @@ class UnusedVariableVisitor(WarningNodeVisitor):
             self.used_variables.add(node.id)
         self.generic_visit(node)
 
+class FunctionVisitor(ast.NodeVisitor):
+    def __init__(self):
+        super().__init__()
+        self.function_visitors = []
+
+    def visit_FunctionDef(self, node):
+        visitor = UnusedVariableVisitor()
+        visitor.visit(node)
+        self.function_visitors.append(visitor)
+        self.generic_visit(node)
+
 class UnusedVariableTestRule(Rule):
     def analyze(self, node):
         warnings = []
-        self.function_visitors = []
+        
+        function_visitor = FunctionVisitor()
+        function_visitor.visit(node)
 
-        self.analyze_function(node)
-
-        # Una vez recopilados todos los visitantes de las funciones, analizamos cada uno para obtener las advertencias
-        for visitor in self.function_visitors:
+        for visitor in function_visitor.function_visitors:
             for var in visitor.assigned_variables:
                 if var not in visitor.used_variables:
                     warnings.append(Warning('UnusedVariable', visitor.assigned_variables[var], f'variable {var} has not been used'))
 
         return warnings
 
-    def analyze_function(self, node):
-        if isinstance(node, ast.FunctionDef):
-            visitor = UnusedVariableVisitor()
-            visitor.visit(node)
-            self.function_visitors.append(visitor)
-
-        # Luego llamamos recursivamente a todos los nodos hijos
-        for child in ast.iter_child_nodes(node):
-            self.analyze_function(child)
-
     @classmethod
     def name(cls):
         return 'not-used-variable'
-
-
