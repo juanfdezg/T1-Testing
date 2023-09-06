15f7eefb61cc41a175a3833d3725aa9b0f3992d7
Juan Fernandez <jfernaandez@uc.cl>
2023-09-05 22:53:43 -0300

    Unused variable ready

--- core/rules/unused_variable.py
+++ core/rules/unused_variable.py
@@ -1,42 +1,47 @@
 from ..rule import *
-import ast
 
-class UnusedVariableVisitor(ast.NodeVisitor):
+class UnusedVariableVisitor(WarningNodeVisitor):
     def __init__(self):
-        super().__init__()
+        # queremos guardar las variables que han sido asignadas y sus líneas respectivas
         self.assigned_variables = {}
+        # guardamos también las variables que han sido usadas en un conjunto (set) para evitar repetidos
         self.used_variables = set()
 
     def visit_Assign(self, node):
+        # revisamos todos los targets, que son las variables a las que se asigna un valor (lo que va a la izquierda del =),
         for target in node.targets:
-            if isinstance(target, ast.Name):
+            if isinstance(target, Name):
+                # guardamos la variable y su línea
                 self.assigned_variables[target.id] = node.lineno
         self.generic_visit(node)
 
     def visit_Name(self, node):
-        if isinstance(node.ctx, ast.Load):
+        # revisamos si la variable se usa
+        if isinstance(node.ctx, Load):
             self.used_variables.add(node.id)
         self.generic_visit(node)
 
-class FunctionVisitor(ast.NodeVisitor):
+# esta clase es para visitar las funciones y guardar los visitors de cada una
+# tuvimos que implementarla porque antes estabamos usando ast.iter_child_nodes, pero está prohibido por enunciado
+class FunctionVisitor(NodeVisitor):
     def __init__(self):
-        super().__init__()
-        self.function_visitors = []
+        self.visitors_funcion = []
 
     def visit_FunctionDef(self, node):
         visitor = UnusedVariableVisitor()
         visitor.visit(node)
-        self.function_visitors.append(visitor)
+        self.visitors_funcion.append(visitor)
         self.generic_visit(node)
 
 class UnusedVariableTestRule(Rule):
     def analyze(self, node):
         warnings = []
         
-        function_visitor = FunctionVisitor()
-        function_visitor.visit(node)
+        visitor_funcion = FunctionVisitor()
+        visitor_funcion.visit(node)
 
-        for visitor in function_visitor.function_visitors:
+        # iteramos sobre los visitors de cada función y revisamos si hay variables que no se usaron
+        for visitor in visitor_funcion.visitors_funcion:
             for var in visitor.assigned_variables:
                 if var not in visitor.used_variables:
                     warnings.append(Warning('UnusedVariable', visitor.assigned_variables[var], f'variable {var} has not been used'))
